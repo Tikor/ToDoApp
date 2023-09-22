ebeb20d6485519914dbe5c6c9822882f30289bc7
Tikor <ceglinski.piotr@gmail.com>
2023-09-22 12:40:24 +0200

    reverted 2 commits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 import './globals.css'
-import '@radix-ui/themes/styles.css'
-
-import { ClerkProvider } from '@clerk/nextjs'
-import { Theme } from '@radix-ui/themes';
-import { Inter } from 'next/font/google'
-
 import type { Metadata } from 'next'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import mountains from '../../public/mountains.jpg'
 import Header from '@/components/header/Header'
@@ -24,26 +20,24 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-          <body className={` ${inter.className}`}>
-            <Theme>        
-              <div className="relative flex flex-col text-neutral-200  m-0 p-0 min-h-screen w-full overflow-hidden">
-                <div className="container mx-auto flex grow flex-col px-4">
-                <Header />
-                {children}
-                </div>
-                <Image
-                  src={mountains}
-                  placeholder="blur"
-                  quality={100}
-                  fill
-                  sizes="100vw"
-                  alt="Mountains"
-                  className="object-cover -z-50 dark:grayscale"
-                />
-              </div>
-            </Theme>        
-          </body>
+        <body className={` ${inter.className}`}>
+        <div className="relative flex flex-col text-neutral-200  m-0 p-0 min-h-screen w-full overflow-hidden">
+          <div className="container mx-auto flex grow flex-col px-4">
+          <Header />
+          {children}
+          </div>
+          <Image
+            src={mountains}
+            placeholder="blur"
+            quality={100}
+            fill
+            sizes="100vw"
+            alt="Mountains"
+            className="object-cover -z-50 dark:grayscale"
+          />
+        </div>
+        </body>
       </html>
-    </ClerkProvider>    
+    </ClerkProvider>
   )
 }
